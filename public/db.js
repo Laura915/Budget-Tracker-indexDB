ca8699911836095750b9af73bab9ae84ab4d1722
Laura <lhernan53112@gmail.com>
2020-07-17 15:47:13 -0400

    Added event listener that checks for online and calls checkDatabase function

--- public/db.js
+++ public/db.js
@@ -4,11 +4,52 @@ let db;
 const request = indexedDB.open("budget", 1);
 
 // creates "pending" schema
-request.onupgradeneeded = function(event) {
-   const db = event.target.result;
-   db.createObjectStore("pending", { autoIncrement: true });
- };
-
-request.onsuccess = event => {
-    console.log(request.result);
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
 };
+
+request.onsuccess = function (event) {
+  db = event.target.result;
+  // check if app is online before reading from db
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+function checkDatabase() {
+  // open a transaction on your pending db
+  const transaction = db.transaction(["pending"], "readwrite");
+  // access your pending object store
+  const store = transaction.objectStore("pending");
+  // get all records from store and set to a variable
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+        .then(response => response.json())
+        .then(() => {
+        // if successful, open a transaction on your pending db
+        const transaction = db.transaction(["pending"], "readwrite");
+
+        // access your pending object store
+        const store = transaction.objectStore("pending");
+
+        // clear all items in your store
+        store.clear();
+
+        });
+    }
+  };
+}
+
+// listen for app coming back online
+window.addEventListener("online", checkDatabase);
