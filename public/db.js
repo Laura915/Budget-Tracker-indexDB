98ddd48f77baf94cba5e62e82e12d85cc9e58587
Laura <lhernan53112@gmail.com>
2020-07-17 11:46:22 -0400

    Created 'pending' table schema with autoIncrement property

--- public/db.js
+++ public/db.js
@@ -1,2 +1,14 @@
+let db;
+
 // creates "budget" database.
 const request = indexedDB.open("budget", 1);
+
+// creates "pending" schema
+request.onupgradeneeded = function(event) {
+   const db = event.target.result;
+   db.createObjectStore("pending", { autoIncrement: true });
+ };
+
+request.onsuccess = event => {
+    console.log(request.result);
+};
